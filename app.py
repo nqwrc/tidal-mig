fc2ad3c05e9032ef256298fe5cbb3bacc9cbf5a8
nqwrc <nicola.pandolfi15@gmail.com>
2024-03-19 16:29:47 +0100

    p

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # return '<a href="/login">Login with Tidal</a>'
-    return '<a href="/login" class="button">Login with Tidal</a>'
+    render_template(index.html)
 
 @app.route('/login')
 def login():
