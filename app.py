aee9c1c39ee27f91003ea95824e38e50c617512d
nqwrc <nicola.pandolfi15@gmail.com>
2024-03-20 07:03:36 +0100

    p

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, redirect, url_for
 import tidalapi
+import webbrowser
 
 app = Flask(__name__)
 
+session_main = tidalapi.Session()
+obj_main, _ = session_main.login_oauth()
+url_main = obj_main.verification_uri_complete
+
 @app.route('/')
 def index():
-    return '<a href="/login">Login with Tidal</a>'
-    # return '<a href="/login" class="button">Login with Tidal</a>'
+    print(url_main)
+    return render_template('index.html', url_main=url_main) 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    session_main = tidalapi.Session()
-    obj_main, _ = session_main.login_oauth()
-    url_main = obj_main.verification_uri_complete
-    return redirect(url_main)
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+
 
 @app.route('/import_favorites')
 def import_favorites():
-    favorites = session.get_user_favorites_tracks()
+    favorites = session_main.get_user_favorites_tracks()
 
     # Store or process the favorites as needed
     return "Favorites imported!"
